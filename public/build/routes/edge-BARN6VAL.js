2cc779125f82c19b0c57d974f4c2142dbcb766a4
Andridge Munene <munenengarw@gmail.com>
2023-07-25 10:01:06 +0300

    Paper

---
+++ public/build/routes/edge-BARN6VAL.js
@@ -0,0 +1,22 @@
+import {
+  __toESM,
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-4QJKEWZT.js";
+
+// app/routes/edge.tsx
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+function Edge() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { style: { fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }, children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h1", { children: "Welcome to Remix@Edge" }, void 0, false, {
+    fileName: "app/routes/edge.tsx",
+    lineNumber: 6,
+    columnNumber: 7
+  }, this) }, void 0, false, {
+    fileName: "app/routes/edge.tsx",
+    lineNumber: 5,
+    columnNumber: 5
+  }, this);
+}
+export {
+  Edge as default
+};
+//# sourceMappingURL=/build/routes/edge-BARN6VAL.js.map
